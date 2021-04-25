a2355d7049e7c6ec7c85d8d92d7866e0ae2868c1
Raghda  Muayad <therose000777@gmail.com>
2021-04-25 03:08:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-animal = ""
+//animal = ""
+
+//function myAnimal() {
+  //return animal
+//}
+
+//function yourAnimal() {
+  // How can we make sure that this function
+  // and the above function both pass?
+  // P.S.: You can't just hard-code 'cat' below
+//  return animal
+//}
+
+//function add2(n) {
+//  return n + two
+
+  // Feel free to move things around!
+  //two = 2
+//}
+
+
+const animal =["dog"];
 
 function myAnimal() {
-  return animal
+  return animal;
 }
-
+const animalOne =["cat"];
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  return animalOne;
 }
 
 function add2(n) {
-  return n + two
+
+  return n + 2;
 
   // Feel free to move things around!
-  two = 2
+
 }
